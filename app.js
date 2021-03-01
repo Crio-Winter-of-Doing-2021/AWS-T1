cb5e1341eb0dc03263d8375e9e0cc615781d8afb
pushpendrakumar <pkc3766@gmail.com>
2021-03-01 19:14:23 +0530

    added basic schedule task and cancel tasks functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const mongoose = require('mongoose');
 const DB = require(__dirname+'/db.js');
+const axios = require('axios');
 
 const app = express();
 app.set('view engine','ejs');
@@ -18,10 +19,8 @@ DB.connection();
 //create or return a pre-created scheduler collection
 const Task = DB.createSchedulerCollection();
 
-//save a task schedule in tasks collection
-// const task=new Task({lambdaURL:"aws-t1",timeDelayInMs:"3000",
-//     taskState:"scheduled"});
-// task.save();
+//tasks maps taskId with its setTimeout() function call
+let tasks = new Map();
 
 app.get("/",function(req,res){
     console.log("Requesting home page");
@@ -29,7 +28,63 @@ app.get("/",function(req,res){
 });
 
 app.get("/schedule",function(req,res){
-    res.render('taskSchedule');
+    res.render('scheduleTask');
+});
+
+app.post("/schedule",function(req,res){
+    const url=req.body.URL;
+    const timeDelay=req.body.timeInMs;
+    const taskState='scheduled';
+    console.log("url: "+url);
+    console.log("timeInMs: "+timeDelay);
+    
+    // schedule the aws lambda task
+    var task = setTimeout(function(){
+        axios.post(url, {
+            'key1':'value1'
+          })
+          .then((response) => {
+            console.log(response);
+          }, (error) => {
+            console.log(error);
+          });
+    },timeDelay);
+
+    //store task details in database
+    const taskInfo = new Task({lambdaURL:url,timeDelayInMs:timeDelay,
+        taskState:taskState});
+        taskInfo.save(function(err,result){ 
+                if (err){ 
+                    console.log(err); 
+                } 
+                else{ 
+                    // need error handling 
+                    // console.log(result);
+                    // console.log(typeof result._id);
+                    let id=JSON.stringify(result._id);
+                    id=id.substring(1,id.length-1);
+                    console.log("id "+id);
+                    tasks.set(id,task);
+                    // console.log(tasks.get(id));
+                    //update state of task to cancelled in db
+                    res.json({'id':result._id});
+                } 
+        });
+    
+});
+
+app.get("/cancel",function(req,res){
+    res.render('cancelTask');
+});
+
+app.post("/cancel",function(req,res){
+    let taskId = req.body.taskId;
+    // console.log("in cancel "+ taskId);
+    //taskId=""+taskId;
+    // console.log(tasks);
+    // console.log('in cancel'+ tasks.get(taskId));
+    clearTimeout(tasks.get(taskId));
+    res.render('cancelTask');
 });
 
 app.listen(3000,function(){
--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+/*connects to database schedulerDB */
 module.exports.connection=function(){
     //connect with database
     mongoose.connect('mongodb://localhost:27017/schedulerDB',{
@@ -15,6 +16,12 @@ module.exports.connection=function(){
     });
 };
 
+
+
+/*
+    creates a collection in schedulerDB with fields
+    {lambdaURL, timeDelayInMs, taskState} 
+*/
 module.exports.createSchedulerCollection=function(){
     //connection();
     const schedulerCollection= mongoose.Schema({
@@ -24,3 +31,42 @@ module.exports.createSchedulerCollection=function(){
     });
     return mongoose.model("task",schedulerCollection);
 };
+
+
+
+
+//connect to database
+//this.connection();
+
+//create or return a pre-created scheduler collection
+// const Task = this.createSchedulerCollection();
+
+
+//save task in database
+/*
+    params:
+    url: aws lambda api url
+    timeDelay: Delay in ms 
+    taskState: state of task when it is saved
+
+    returns taskId after saving task in database.
+*/
+// module.exports.saveTask = function(url,timeDelay,taskState){
+//     const task=new Task({lambdaURL:url,timeDelayInMs:timeDelay,
+//     taskState:taskState});
+//     task.save(function(err,result){ 
+//             if (err){ 
+//                 console.log(err); 
+//             } 
+//             else{ 
+//                 console.log(result);
+//                 console.log(result._id);
+//                 return result._id;
+//                 tasks.set(result._id,task)
+//             } 
+//     });
+// };
+
+
+
+module.exports
