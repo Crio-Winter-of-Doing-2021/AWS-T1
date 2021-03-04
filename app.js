172532c7be3d22e6715f501f4ee3c485ae435187
pushpendrakumar <pkc3766@gmail.com>
2021-03-04 15:02:51 +0530

    added functions saveScheduledTask, getParams

--- app.js
+++ app.js
@@ -21,7 +21,60 @@ const TaskModel = DB.createSchedulerCollection();
 
 //tasks maps taskId with its setTimeout() function call
 let tasks = new Map();
+//id of the scheduled task
+let id='';
 
+/*               Utility Functions for app.js                */
+//get params passed with the scheduled task
+function getParams(req)
+{
+    let ind=0;
+    var params=[];
+    let checkbox=req.body.checkbox0;
+    while(typeof checkbox!="undefined")
+    {
+        let temp='';
+        if(checkbox=="on")
+        {
+            temp='key'+ind;
+            const key=req.body[temp];
+            temp='value'+ind;
+            const value=req.body[temp];
+            if(key!=""||value!="")
+            {
+                params.push({key,value});
+            }
+        }
+        ind++;
+        temp='checkbox'+ind;
+        checkbox=req.body[temp];
+    }
+    return params;
+}
+
+//save a scheduled task in database and add task to the map tasks
+function saveScheduledTask(res,url,timeDelay,taskState,task)
+{
+    const taskInfo = new TaskModel({lambdaURL:url,timeDelayInMs:timeDelay,
+                                    taskState:taskState});
+    taskInfo.save(function(err,result){ 
+        if (err){ 
+            console.log(err); 
+        } 
+        else
+        { 
+            id=result._id.toString();
+            console.log("id "+id);
+            tasks.set(id,task);
+           
+            res.json({"success":
+            "Your task has been succesfully scheduled",
+            'id':result._id});
+        } 
+    });
+}
+
+/*                          Routes                             */
 app.get("/",function(req,res){
     console.log("Requesting home page");
     res.render("home");
@@ -31,53 +84,27 @@ app.get("/schedule",function(req,res){
     res.render('scheduleTask');
 });
 
+
+
 app.post("/schedule",function(req,res){
     const url=req.body.URL;
     const timeDelay=req.body['timeInMs'];
     const taskState='initial stage';
     console.log("url: "+url);
     console.log("timeInMs: "+timeDelay);
-    let ind=0;
-    var params=[];
-    let checkbox=req.body.checkbox0;
-    // console.log('checkbox '+checkbox0);
-    // console.log(typeof checkbox0);
-    while(typeof checkbox!="undefined")
-    {
-        let temp='';
-        //console.log(checkbox);
-        if(checkbox=="on")
-        {
-            //console.log('inside');
-            temp='key'+ind;
-            const key=req.body[temp];
-            temp='value'+ind;
-            const value=req.body[temp];
-            //console.log(key+" "+value);
-            if(key!=""||value!="")
-            {
-                params.push({key,value});
-            }
-        }
-        ind++;
-        temp='checkbox'+ind;
-        checkbox=req.body[temp];
-        //console.log("checkbox "+checkbox)
-    }
-    //console.log('params: '+params);
-    // res.send('submitted');
-    let id='';
+    let params = getParams(req);
+    //let id='';
     
     // schedule the aws lambda task
     var task = setTimeout(function(){
-        //TODO: update in database taskState to running
+        //update in database taskState to running
         DB.updateTaskState(TaskModel,id,'running');
         axios.post(url,params)
           .then((response) => {
                 //edge case: immediately executing tasks i.e timeDelay 0ms 
                 if(tasks.has(id))
                 {
-                    //TODO: update in database taskState to completed
+                    //update in database taskState to completed
                     DB.updateTaskState(TaskModel,id,'completed');
                     console.log('Task '+id+ '  deleted succefully from map tasks');
                     tasks.delete(id);
@@ -86,37 +113,15 @@ app.post("/schedule",function(req,res){
                 }
             
           }, (error) => {
-                //TODO: update in database taskState to failed 
+                //update in database taskState to failed 
                 DB.updateTaskState(TaskModel,id,'failed');
                 console.log('error occured while executing task')
                 console.log(error);
           });
     },timeDelay);
 
-    //store task details in database
-    const taskInfo = new TaskModel({lambdaURL:url,timeDelayInMs:timeDelay,
-                                taskState:'scheduled'});
-    taskInfo.save(function(err,result){ 
-            if (err){ 
-                console.log(err); 
-            } 
-            else{ 
-                // need error handling 
-                // console.log(result);
-                // console.log(typeof result._id);
-                // let id=JSON.stringify(result._id);
-                // id=id.substring(1,id.length-1);
-                id=result._id.toString();
-                console.log("id "+id);
-                tasks.set(id,task);
-                // console.log(tasks.get(id));
-                //update state of taskState to scheduled in db
-                //DB.updateTaskState(TaskModel,id,'scheduled');
-                res.json({"success":
-                "Your task has been succesfully scheduled",
-                'id':result._id});
-            } 
-    });
+    //store scheduled task details in database and task to map tasks
+    saveScheduledTask(res,url,timeDelay,'scheduled',task);
     
 });
 
@@ -126,22 +131,17 @@ app.get("/cancel",function(req,res){
 
 app.post("/cancel",function(req,res){
     let taskId = req.body.taskId;
-    // console.log("in cancel "+ taskId);
-    //taskId=""+taskId;
-    // console.log(tasks);
-    // console.log('in cancel'+ tasks.get(taskId));
     if(tasks.has(taskId))
     {
         clearTimeout(tasks.get(taskId));
         tasks.delete(taskId);
-        //TODO: update taskState to cancelled in DB
+        //update taskState to cancelled in DB
         DB.updateTaskState(TaskModel,taskId,'cancelled');
         res.json({'success':'Task with id '+ taskId+ ' has been deleted successfully'});
     }
     else{
         res.json({'error':'Task with id '+ taskId+' is either already completed or not scheduled'});
-    }
-    
+    }    
     //res.render('cancelTask');
 });
 
--- db.js
+++ db.js
@@ -39,10 +39,7 @@ module.exports.updateTaskState = function(TaskModel,id,taskState){
                 console.log('could not update');
             }
             else{
-                // console.log(id);
-                // console.log(taskState);
                 console.log('successfully updated taskState to: '+taskState);
-                //console.log(taskState);
             }
             
     });
@@ -63,28 +60,6 @@ module.exports.updateTaskState = function(TaskModel,id,taskState){
 // const Task = this.createSchedulerCollection();
 
 
-//save task in database
-/*
-    params:
-    url: aws lambda api url
-    timeDelay: Delay in ms 
-    taskState: state of task when it is saved
 
-    returns taskId after saving task in database.
-*/
-// module.exports.saveTask = function(url,timeDelay,taskState){
-//     const task=new Task({lambdaURL:url,timeDelayInMs:timeDelay,
-//     taskState:taskState});
-//     task.save(function(err,result){ 
-//             if (err){ 
-//                 console.log(err); 
-//             } 
-//             else{ 
-//                 console.log(result);
-//                 console.log(result._id);
-//                 return result._id;
-//                 tasks.set(result._id,task)
-//             } 
-//     });
-// };
+
 
